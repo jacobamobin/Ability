369082a548f0e8ba7894b11bdf57e0ad135dbc7a
Jacob Mobin <jacobamobin@gmail.com>
2025-03-21 23:00:03 -0400

    Initial commit

---
+++ Ability/AbilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbilityApp.swift
+//  Ability
+//
+//  Created by Jacob Mobin on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct AbilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ability/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ability
+//
+//  Created by Jacob Mobin on 3/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
